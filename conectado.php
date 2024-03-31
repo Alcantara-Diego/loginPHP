35c46e28e63889baa2f5c3c2ad31b5b753c2469c
Diego Alcântara <ddag0309@gmail.com>
2024-03-31 19:33:25 -0300

    style: Adiciona estilo na página de usuários conectados

--- conectado.php
+++ conectado.php
@@ -17,9 +17,6 @@ session_start();
 
         header("location: conectado.php");
         exit();
-
-
-
     }
 
 
@@ -32,24 +29,61 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style/conectado.css">
     <title>Usuário conectado</title>
 </head>
 <body>
-    <?php
-        echo "<h1>Bem-vindo " .$_SESSION["nome"] . "</h1>";
-    ?>
-  <h3>Você está conectado!</h3>
+    <header>
+        <nav>
+            <div>
+                
+                
+                <h3>Notas PHP</h3>
+            </div>
+            
+                  <a href="index.php">Sair</a>
+        </nav>
 
-  <?php
-    echo "<form method='get'> 
-    <textarea name='notaSalva' cols='30' rows='4'>" . $_SESSION["notas"] . "</textarea>
-    <br>
-    <input type='submit' value='Atualizar recado'>
-    <br>
-    ";
-  ?>
-  
-    <a href="index.php">Sair</a>
+
+        <div class="usuarioInfo">
+            <?php
+            echo "<h2>Bem-vindo " .$_SESSION["nome"] . "</h2>";
+            ?>
+
+            <h3>Você está conectado!</h3>
+        </div>
+
+    </header>
+
+    
+    <main>
+    
+        <form method="get" class="notaPrincipal nota">
+    
+            <?php
+                echo "<h4 class='titulo'>" . $_SESSION["nome"] . "</h4>
+    
+                <textarea class='recado' name='notaSalva' cols='15' rows='4'>" . $_SESSION["notas"] . "</textarea>
+                <input type='submit' value='Atualizar recado'>
+                ";
+            ?>
+    
+        </form>
+    
+        <div class="notasContainer">
+    
+            <div class="notaCriada nota">
+                <header>
+                    <h4 class='titulo'>Diego Alcantara</h4>
+                    <p class='recado'>Estou aprendendo PHP e criei todo um sistema de exibir notinha só pro login que estou aprendendo ter algum propósito...</p>
+                </header>
+    
+                <p class='email'>diego@email.com</p>
+            </div>
+    
+    
+        </div>
+    </main>
     
 </body>
 </html>
